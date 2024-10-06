399887ab758762f25e25e5c07163b58296271291
tenkyuu <hello@tenkyuu.dev>
2024-10-06 11:57:32 +0800

    mobile complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-function menuToggleOpen() {
-  document.getElementById("menu").style.width = "100vw";
-}
-function menuToggleClose() {
-  document.getElementById("menu").style.width = "0";
-}
+const mobile_menu = document.querySelector(".mobile-menu");
+const nav_menu = document.querySelector(".nav-menu");
+
+mobile_menu.addEventListener("click", () => {
+  mobile_menu.classList.toggle("active");
+  nav_menu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  mobile_menu.classList.remove("active"); 
+  nav_menu.classList.remove("active");
+}))
